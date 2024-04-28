d2964ad322c36909311308c28eadeddfd155b4bc
Rafael Carneiro Pregardier <rcpregardier@inf.ufsm.br>
2024-04-28 18:08:27 -0300

    questao 8

---
+++ TRABALHO 1/QUESTÃO 8/QuebraCabeca.py
@@ -0,0 +1,45 @@
+class QuebraCabeca:
+    def __init__(self, origem, objetivo, funcaoCusto):
+        self.origem = origem
+        self.objetivo = objetivo
+        self.funcaoCusto = funcaoCusto
+
+    def estadoInicial(self):
+        return self.origem
+    
+    def testeObjetivo(self, estado):
+        return estado == self.objetivo
+    
+    def funcaoSucessora(self, estado):
+        # Encontra a posição do zero
+        posicao_zero = estado.index(0)
+        
+        # Encontra as posições possíveis para mover o zero
+        posicoes_possiveis = []
+        if posicao_zero % 3 != 0:
+            posicoes_possiveis.append(posicao_zero - 1)
+        if posicao_zero % 3 != 2:
+            posicoes_possiveis.append(posicao_zero + 1)
+        if posicao_zero > 2:
+            posicoes_possiveis.append(posicao_zero - 3)
+        if posicao_zero < 6:
+            posicoes_possiveis.append(posicao_zero + 3)
+        
+        # Gera os sucessores
+        sucessores = []
+        for posicao in posicoes_possiveis:
+            sucessor = estado.copy()
+            sucessor[posicao_zero], sucessor[posicao] = sucessor[posicao], sucessor[posicao_zero]
+            sucessores.append((sucessor[posicao], sucessor))
+        
+        return sucessores
+    
+    def heuristica(self, estado):
+        return sum([1 if estado[i] != self.objetivo[i] else 0 for i in range(9)])
+    
+    def custo(self, estado, acao):
+        if self.funcaoCusto == 1:
+            return 1
+        
+        if self.funcaoCusto == 2:
+            return self.heuristica(estado)
---
+++ TRABALHO 1/QUESTÃO 8/main.py
@@ -0,0 +1,32 @@
+import BuscaGulosa, QuebraCabeca
+
+def main():
+    problema = QuebraCabeca.QuebraCabeca([1, 3, 4,
+                                          8, 2, 5,
+                                          7, 6, 0],
+                                         [1, 2, 3,
+                                          8, 0, 4,
+                                          7, 6, 5], 1)
+
+    busca_gulosa = BuscaGulosa.BuscaGulosa()
+
+    busca_gulosa.buscar(problema)
+    
+    busca_gulosa.mostra_caminho()
+    
+
+    problema = QuebraCabeca.QuebraCabeca([1, 3, 4,
+                                          8, 2, 5,
+                                          7, 6, 0],
+                                         [1, 2, 3,
+                                          8, 0, 4,
+                                          7, 6, 5], 2)
+    
+    busca_gulosa = BuscaGulosa.BuscaGulosa()
+
+    busca_gulosa.buscar(problema)
+
+    busca_gulosa.mostra_caminho()
+
+if __name__ == '__main__':
+    main()
