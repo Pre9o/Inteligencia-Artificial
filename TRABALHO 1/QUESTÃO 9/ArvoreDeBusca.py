dac2f99e0fce7a62771041fb9581a4ea17d42639
Rafael Carneiro Pregardier <rcpregardier@inf.ufsm.br>
2024-04-29 09:40:48 -0300

    add alphabeta questao 9

--- TRABALHO 1/QUESTÃO 9/ArvoreDeBusca.py
+++
@@ -1,7 +0,0 @@
-class ArvoreDeBusca:
-    def __init__(self, folhas):
-        self.folhas = folhas
-        self.raiz = None
-
-    def retornaFolhas(self):
-        return self.folhas
--- TRABALHO 1/QUESTÃO 9/MinMax.py
+++ TRABALHO 1/QUESTÃO 9/MinMax.py
@@ -21,38 +21,66 @@ def minimax(Nodo, profundidade, maximizando_jogador):
             min_eval = min(min_eval, eval)
         Nodo.valor = min_eval
         return min_eval
+    
+def poda_alpha_beta(Nodo, profundidade, alpha, beta, maximizando_jogador):
+    if profundidade == 0 or len(Nodo.filhos) == 0:
+        return Nodo.valor
+    
+    if maximizando_jogador:
+        max_eval = float('-inf')
+        for filho in Nodo.filhos:
+            eval = poda_alpha_beta(filho, profundidade - 1, alpha, beta, False)
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        Nodo.valor = max_eval
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for filho in Nodo.filhos:
+            eval = poda_alpha_beta(filho, profundidade - 1, alpha, beta, True)
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        Nodo.valor = min_eval
+        return min_eval
+    
+def arvore_preenchida():
+    # Construindo a árvore
+    root = Nodo(0)
 
-# Construindo a árvore
-root = Nodo(0)
-
-root.filhos = [
-    Nodo(0),
-    Nodo(0)
-]
-
-contador = 0
-
-for i, filho in enumerate(root.filhos):
-    filho.filhos = [
+    root.filhos = [
         Nodo(0),
         Nodo(0)
     ]
 
-    for j, neto in enumerate(filho.filhos):
-        neto.filhos = [
+    contador = 0
+
+    for i, filho in enumerate(root.filhos):
+        filho.filhos = [
             Nodo(0),
             Nodo(0)
         ]
 
-        for k, bisneto in enumerate(neto.filhos):
-            bisneto.filhos = [
+        for j, neto in enumerate(filho.filhos):
+            neto.filhos = [
                 Nodo(0),
                 Nodo(0)
             ]
 
-            for l, tataraneto in enumerate(bisneto.filhos):
-                tataraneto.valor = [20, 33, -45, 31, 24, 25, -10, 20, 40, -25, 18, -42, 24, -19, 36, -41][contador]
-                contador += 1
+            for k, bisneto in enumerate(neto.filhos):
+                bisneto.filhos = [
+                    Nodo(0),
+                    Nodo(0)
+                ]
+
+                for l, tataraneto in enumerate(bisneto.filhos):
+                    tataraneto.valor = [20, 33, -45, 31, 24, 25, -10, 20, 40, -25, 18, -42, 24, -19, 36, -41][contador]
+                    contador += 1
+
+    return root
 
 # Aplicando o algoritmo Minimax alternando os jogadores
 def alternar_minimax(Nodo, profundidade):
@@ -61,9 +89,6 @@ def alternar_minimax(Nodo, profundidade):
     else:
         minimax(Nodo, profundidade, True)   # Minimizando
 
-# Aplicando o algoritmo Minimax alternando os jogadores
-alternar_minimax(root, 5)
-
 # Função para imprimir a árvore
 def print_arvore(Nodo, profundidade=0):
     if Nodo:
@@ -71,6 +96,19 @@ def print_arvore(Nodo, profundidade=0):
         for filho in Nodo.filhos:
             print_arvore(filho, profundidade + 2)
 
+root = arvore_preenchida()
+
+# Aplicando o algoritmo Minimax alternando os jogadores
+alternar_minimax(root, 5)
+
 # Imprimindo a árvore preenchida
-print("Árvore preenchida:")
+print("Árvore preenchida MiniMax:")
+print_arvore(root)
+
+root = arvore_preenchida()
+
+poda_alpha_beta(root, 5, float('-inf'), float('inf'), True)
+
+# Imprimindo a árvore preenchida
+print("Árvore preenchida Poda AlphaBeta:")
 print_arvore(root)
